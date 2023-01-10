3c959f5c22cb888874c76c38347b2699e5f8233d
aju100 <ajutamang10@outlook.com>
2023-01-10 23:20:15 +0545

    feat: add prettier with CI config & add gitignore

--- bin/index.js
+++ bin/index.js
@@ -5,8 +5,8 @@ const figlet = require("figlet");
 const program = new Command();
 console.log(figlet.textSync("Timezone"));
 program
-    .version("1.0.0")
-    .description("Timezone CLI for managing the times for remote team")
-    .parse(process.argv);
+  .version("1.0.0")
+  .description("Timezone CLI for managing the times for remote team")
+  .parse(process.argv);
 const options = program.opts();
 //# sourceMappingURL=index.js.map
